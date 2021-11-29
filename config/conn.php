852c7718f2b3a73bacda263f2d11b06afe4bf273
RAHMAT DIAN RIFA'I <72428777+Dianrifa@users.noreply.github.com>
2021-11-29 14:11:39 +0700

    Add files via upload

---
+++ config/conn.php
@@ -0,0 +1,12 @@
+<?php
+
+// SQL server connection information
+$sql_details = array(
+    'user' => 'root',
+    'pass' => '',
+    'db'   => 'dummy',
+    'host' => 'localhost'
+);
+
+$con = $sql_details;
+?>
