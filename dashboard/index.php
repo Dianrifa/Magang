ae23add21f9e540f421f0164e9d0f01ebd7a6de6
RAHMAT DIAN RIFA'I <72428777+Dianrifa@users.noreply.github.com>
2021-11-29 14:12:20 +0700

    Add files via upload

---
+++ dashboard/index.php
@@ -0,0 +1,10 @@
+<?php include_once('../header.php')?>
+			<div class="row">
+        		<div class="col-lg-12">
+            		<h1>Dashboard</h1>
+            			<p>Selamat Datang <?=isset($_SESSION['user'])?> di Web Data Covid 19 Indonesia</p>
+                		<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Klik</a>
+       			</div>
+    		</div>
+<?php include_once('../footer.php'); 
+?>
